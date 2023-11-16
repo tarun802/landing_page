3546b9323d7e5d9e3ad58e06dd869826cc574f23
tarun802 <144223140+tarun802@users.noreply.github.com>
2023-11-16 20:46:11 +0530

    Add files via upload

---
+++ src/data/HeroData.js
@@ -0,0 +1,62 @@
+export const heroOne = {
+	id: 'projects',
+	topLine: {
+		text: 'True Crime',
+	},
+	headline: 'Crime Junkie',
+	description: 'Crime Junkie is a weekly true crime podcast dedicated to giving you a fix. ',
+	buttonLabel: 'View More',
+	imgStart: 'start',
+	img: './images/content/crimejunkie.jpeg',
+	backgroundColor: 'linear-gradient(	150deg,#c99fff -20%,#4a4eff)',
+	start: 'true',
+};
+
+export const heroTwo = {
+	reverse: true,
+	topLine: {
+		text: 'Comedy',
+	},
+	headline: 'Off Menu',
+	description:
+		"Comedians Ed Gamble and James Acaster invite special guests into their magical restaurant to each choose their favourite meal.Ever wanted to eat your dream meal? It's time to order Off Menu.",
+	buttonLabel: 'View More',
+
+	linkTo: '/more',
+	imgStart: 'start',
+	img: './images/content/off_menu.jpeg',
+	backgroundColor: 'linear-gradient(	140deg	,#ffaf73 30%,#fffecc 120%)',
+	start: 'true',
+};
+
+export const heroThree = {
+	topLine: {
+		text: 'Educational',
+	},
+	headline: "Stuff You Should Know ",
+	description:
+		'Each episode, the hosts focus on explaining and debunking a particular theme, place, topic, or item. Curious about whether Bigfoot could actually be real and where it came from? Stuff You Should Know has you covered. ',
+	buttonLabel: 'View More',
+
+	linkTo: '/download',
+	imgStart: '',
+	backgroundColor: 'linear-gradient(150deg, #ff6961, #ff8c69)',
+	img: './images/content/sysk.jpeg',
+	start: 'true',
+};
+
+export const heroFour = {
+	reverse: true,
+	topLine: {
+		text: 'Educational',
+	},
+	headline: 'Today Explained',
+	description: "Today, Explained is Vox's daily news explainer podcast. Hosts Sean Rameswaram and Noel King will guide you through the most important stories of the day.",
+	buttonLabel: 'View More',
+
+	linkTo: '/more',
+	imgStart: '',
+	backgroundColor: 'linear-gradient(150deg, #ffd700, #ffec8b)',
+	img: './images/content/Today.avif',
+	start: 'true',
+};
